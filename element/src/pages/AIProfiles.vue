4b905ba04fdd0a0e87f8550ee1a43d61fdeded40
Kyle <kyletanemura@gmail.com>
2017-04-18 18:49:58 -0700

    Populates AI Profiles with data

--- element/src/pages/AIProfiles.vue
+++ element/src/pages/AIProfiles.vue
@@ -28,42 +28,26 @@
             style="width: 100%"
             max-height="250">
             <el-table-column
-              fixed
-              prop="date"
-              label="Date"
-              width="150">
-            </el-table-column>
-            <el-table-column
               prop="name"
-              label="Name"
-              width="120">
+              label="Name">
             </el-table-column>
             <el-table-column
-              prop="state"
-              label="State"
-              width="120">
+              prop="aggression"
+              label="Risk Tendency">
             </el-table-column>
             <el-table-column
-              prop="city"
-              label="City"
-              width="120">
+              prop="trading"
+              label="Make Trades">
             </el-table-column>
             <el-table-column
-              prop="address"
-              label="Address"
-              width="300">
-            </el-table-column>
-            <el-table-column
-              prop="zip"
-              label="Zip"
-              width="120">
+              prop="focus"
+              label="Team Focus">
             </el-table-column>
             <el-table-column
               fixed="right"
-              label="Operations"
-              width="120">
+              label="Operations">
               <template scope="scope">
-                <el-button @click="handleClick" type="text" size="small">Detail</el-button>
+                <el-button @click="handleClick" type="text" size="small">Details</el-button>
                 <el-button type="text" size="small">Edit</el-button>
               </template>
             </el-table-column>
@@ -83,39 +67,50 @@
     },
     data() {
       return {
-        tableData: [{
-          date: '2016-05-03',
-          name: 'Tom',
-          state: 'California',
-          city: 'Los Angeles',
-          address: 'No. 189, Grove St, Los Angeles',
-          zip: 'CA 90036',
-          tag: 'Home'
-        }, {
-          date: '2016-05-02',
-          name: 'Tom',
-          state: 'California',
-          city: 'Los Angeles',
-          address: 'No. 189, Grove St, Los Angeles',
-          zip: 'CA 90036',
-          tag: 'Office'
-        }, {
-          date: '2016-05-04',
-          name: 'Tom',
-          state: 'California',
-          city: 'Los Angeles',
-          address: 'No. 189, Grove St, Los Angeles',
-          zip: 'CA 90036',
-          tag: 'Home'
-        }, {
-          date: '2016-05-01',
-          name: 'Tom',
-          state: 'California',
-          city: 'Los Angeles',
-          address: 'No. 189, Grove St, Los Angeles',
-          zip: 'CA 90036',
-          tag: 'Office'
-        }]
+        tableData: [
+          {
+            name: 'The Brady Bot',
+            aggression: 7,
+            trading: 'True',
+            focus: 'Run Heavy'
+          },
+          {
+            name: 'Predicti-ball',
+            aggression: 4,
+            trading: 'True',
+            focus: 'Arial Attack'
+          },
+          {
+            name: 'Gridiron Guru',
+            aggression: 8,
+            trading: 'True',
+            focus: 'Twin TE'
+          },
+          {
+            name: 'Smashmouth',
+            aggression: 3,
+            trading: 'False',
+            focus: 'Run Heavy'
+          },
+          {
+            name: 'Weekend Warrior',
+            aggression: 5,
+            trading: 'False',
+            focus: 'Run Heavy'
+          },
+          {
+            name: 'The Admrial',
+            aggression: 10,
+            trading: 'False',
+            focus: 'Arial Attack'
+          },
+          {
+            name: 'Turtle',
+            aggression: 2,
+            trading: 'True',
+            focus: 'Run Heavy'
+          },
+        ]
       }
     }
   }
