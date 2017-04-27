ec8914da2160b2237026196fb68275e274dcd696
Kyle <kyletanemura@gmail.com>
2017-04-27 10:36:54 -0700

    Adds blank login to template

---
+++ element/src/pages/Login.vue
@@ -0,0 +1,18 @@
+
+  firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
+  		// Fail
+  		var errorCode = error.code;
+  		var errorMessage = error.message;
+  		
+  });
+
+ <template>
+  <div class="app-dashboard">
+    <div class="col-sm-12 col-md-12">
+      <h2>Login</h2>
+      <p>
+        login
+      </p>
+    </div>
+  </div>
+</template>
