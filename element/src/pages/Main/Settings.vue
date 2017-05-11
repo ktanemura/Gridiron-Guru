48750363a42355780aa9e067ec22541e658008ae
Kyle <kyletanemura@gmail.com>
2017-05-11 10:40:54 -0700

    Adds yahopo auth request params
    Need to construct query strin

--- element/src/pages/Main/Settings.vue
+++ element/src/pages/Main/Settings.vue
@@ -8,4 +8,16 @@ function getParameterByName(name, url) {
     if (!results[2]) return '';
     return decodeURIComponent(results[2].replace(/\+/g, " "));
 }
+
+export default {
+  data() {
+    return {
+    	authRequest: {
+    		url: 'https://api.login.yahoo.com/oauth2/request_auth?',
+    		client_id: 'dj0yJmk9UUdja0lOdmo0dlhXJmQ9WVdrOWJIRjBVRFJSTjJzbWNHbzlNQS0tJnM9Y29uc3VtZXJzZWNyZXQmeD05Mw--',
+    		redirect_uri: 'https://localhost:8010',
+    		response_type: 'token'
+    	}
+    };
+  },
 </script>
