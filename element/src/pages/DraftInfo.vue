307e0c5faf6a4ee8201d5b8b1f46f45f5c0eba11
ktanemura <kyletanemura@gmail.com>
2017-05-21 13:15:12 -0700

    Adds draft info page

---
+++ element/src/pages/DraftInfo.vue
@@ -0,0 +1,102 @@
+<template>
+  <div id="draft">
+    <main-header></main-header>
+    <div class="main-cnt">
+      <el-row :gutter="20">
+        <el-col :span="12" :offset="6">
+          <el-form :model="draftInfoForm" ref="draftInfoForm" label-width="150px" class="demo-ruleForm">
+            <el-form-item
+              label="Number of Teams"
+              prop="numTeams">
+                <el-input-number v-model="draftInfoForm.teams" :min="4" :max="16"></el-input-number>
+            </el-form-item>
+            <el-form-item
+              label="Number of Rounds"
+              prop="numRounds">
+                <el-input-number v-model="draftInfoForm.rounds" :min="15" :max="20"></el-input-number>
+            </el-form-item>
+            <el-form-item>
+              <router-link :to="{ path: 'login' }" tag="el-button">Back to Login</router-link>
+              <el-button type="primary" @click="submitForm(draftInfoForm.teams, draftInfoForm.rounds)">Draft</el-button>
+            </el-form-item>
+          </el-form>
+        </el-col>
+      </el-row>
+    </div>
+  </div>
+</template>
+
+<script type="text/javascript">
+  export default {
+  data() {
+    return {
+      draftInfoForm: {
+        teams: 0,
+        rounds: 0,
+      },
+    };
+  },
+  methods: {
+    submitForm(numTeams, numRounds) {
+      this.$router.push({ path: '/draft', params: { rounds: numRounds, teams: numTeams }})
+    },
+  },
+}
+</script>
+
+<style>
+html, 
+body {
+    height: 100%;
+}
+
+body {
+  margin: 0 0;
+  font-family: 'Helvetica Neue',Helvetica,'PingFang SC','Hiragino Sans GB','Microsoft YaHei',SimSun,sans-serif;
+}
+
+h1 {
+  font-weight: 400;
+}
+
+h2 {
+  font-size: 28px;
+  font-weight: 400;
+  color: #1f2f3d;
+  margin: 0;
+}
+
+h3 {
+  font-size: 22px;
+  font-weight: 400;
+  color: #1f2f3d;
+  margin: 45px 0 15px;
+}
+
+a {
+  text-decoration: none;
+}
+
+p {
+  font-size: 14px;
+  color: #5e6d82;
+  line-height: 1.5em;
+}
+
+#app {
+  margin-left: 30px;
+  margin-right: 30px;
+}
+
+.el-row {
+  margin-top: 10px;
+  margin-bottom: 14px;
+}
+.el-row-breadcrumbs {
+  margin-top: 6px;
+}
+
+.el-card {
+  box-shadow: none;
+}
+</style>
