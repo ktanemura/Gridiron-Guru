f7ddb0076f8533c0c4d86573b53db358078174a7
ktanemura <kyletanemura@gmail.com>
2017-05-21 14:44:12 -0700

    Moves signup page to electron folder
    Fixes initial errors
    Removes useless domain fields

--- gridiron-electron/app/src/renderer/pages/Main/SignUp.vue
+++ gridiron-electron/app/src/renderer/pages/Main/SignUp.vue
@@ -21,6 +21,97 @@
           </div>
         </el-col>
       </el-row>
+<el-form :model="signUpForm" :rules="rules" ref="signUpForm" label-width="150px" class="demo-signUpForm">
+  <el-form-item label="Name" prop="name">
+    <el-input v-model="signUpForm.name" style="width: 50%;"></el-input>
+  </el-form-item>
+  <el-form-item label="Email" prop="email">
+    <el-input type="email" v-model="signUpForm.email" style="width: 50%;"></el-input>
+  </el-form-item>
+  <el-form-item label="Gridiron Username" prop="username">
+    <el-input v-model="signUpForm.username" style="width: 50%;"></el-input>
+  </el-form-item>
+  <el-form-item label="Gridiron Password" prop="password">
+    <el-input type="password" v-model="signUpForm.password" style="width: 50%;"></el-input>
+  </el-form-item>
+  <el-form-item>
+    <el-button type="primary" @click="submitForm('signUpForm', signUpForm.email, signUpForm.password)">Sign Up</el-button>
+    <el-button @click="resetForm('signUpForm')">Reset</el-button>
+  </el-form-item>
+</el-form>
+<router-link :to="'login'" style="text-decoration: none;"><el-button>Back</el-button></router-link>
     </div>
   </div>
 </template>
+<script>
+  import firebase from 'firebase'
+  export default {
+    data () {
+      return {
+        signUpForm: {
+          name: '',
+          email: '',
+          username: '',
+          password: ''
+        },
+        rules: {
+          name: [
+            { required: true, message: 'Please input your name', trigger: 'blur' }
+          ],
+          email: [
+            { required: true, message: 'Please input your email', trigger: 'blur' }
+          ],
+          username: [
+            { required: true, message: 'Please input your new Gridiron username', trigger: 'blur' }
+          ],
+          password: [
+            { required: true, message: 'Please input your new password', trigger: 'blur' }
+          ]
+        }
+
+/*        dynamicValidateForm: {
+          domains: [{
+            key: 1,
+            value: ''
+          }],
+          yahooEmail: ''
+        }
+*/
+
+      }
+    },
+    methods: {
+      submitForm (formName, email, password) {
+        console.log('creating new user...')
+
+        firebase.auth().createUserWithEmailAndPassword(email, password).then(function (firebaseUser) {
+          console.log('success')
+          // this.$router.push('login')
+        })
+        .catch(function (error) {
+          // Handle Errors here.
+          var errorCode = error.code
+          var errorMessage = error.message
+          if (errorCode === 'auth/account-exists-with-different-credential') {
+            alert('Email already associated with another account.')
+          } else {
+            alert(errorMessage)
+          }
+          console.log(error)
+        })
+
+        /* this.$refs[formName].validate((valid) => {
+          if (valid) {
+            alert('submit!');
+          } else {
+            console.log('error submit!!');
+            return false;
+          }
+        }); */
+      },
+      resetForm (formName) {
+        this.$refs[formName].resetFields()
+      }
+    }
+  }
+</script>
